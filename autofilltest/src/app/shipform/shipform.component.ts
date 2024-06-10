3bdf374759cbada874e29239dba88aa042ad0810
Rushikesh Giri <girirushikesh2@gmail.com>
2024-06-10 20:15:54 +0530

    removed unnessary code

--- autofilltest/src/app/shipform/shipform.component.ts
+++ autofilltest/src/app/shipform/shipform.component.ts
@@ -1,9 +1,6 @@
-import { Component, ViewChild, Input, OnInit, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { TitleCasePipe } from '@angular/common';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { MatDrawer } from '@angular/material/sidenav';
-import { MatDialog } from '@angular/material/dialog';
 import { AddressModel, EnrollmentModel } from '../model';
 import { AppConstants } from '../constants';
 
@@ -15,27 +12,14 @@ import { AppConstants } from '../constants';
   styleUrls: ['./shipform.component.css']
 })
 export class ShipformComponent implements OnInit {
- 
-  @Input() address!: AddressModel;
-  @Output() messageEvent: EventEmitter<any> = new EventEmitter();
-  @Output() isShppingDrawerOpened = new EventEmitter<{ isShppingDrawerOpened: boolean }>();
-  @Input() isNoDevicePresent: boolean = false;
 
   public shippingForm!: FormGroup;
   autoFillState = new FormControl();
   states = AppConstants.States;
-  successMessage: string = "";
-  errorMessage: string = "";
   addressModel = new AddressModel();
-  enrollmentLocalStorage: any;
-  loadSavedAddressComponent = false;
-  @ViewChild('shippingList') shippingList!: MatDrawer;
-  disableContinue: boolean = false;
   firstNameMaxLength: number = 20;
   lastNameMaxLength: number = 19;
-  enrollmentModel = new EnrollmentModel();
-  savedAddressInfo: any;  
-  isMatDrawerOpen: boolean = false;
+  demo = new EnrollmentModel();
 
   isEnrollment: string = 'true';
 
@@ -43,14 +27,9 @@ export class ShipformComponent implements OnInit {
 
   ngOnInit() {
     this.createShippingForm();
-
-    // Fix for Auto Fill state Drop down.
     this.autoFillState.valueChanges.subscribe((selectedValue) => {
       this.shippingForm.controls['State'].setValue(selectedValue);
-    });
-
-    this.getSelectedAddress({ selectedAddress: this.address });
-  
+    });  
   }
 
   public hasError = (controlName: string, errorName: string) => {
@@ -58,83 +37,28 @@ export class ShipformComponent implements OnInit {
   }
 
   createShippingForm() {
-    if (this.isEnrollment == "true") {
-      this.prePopulateShippingInfo();
-    }
 
     this.shippingForm = new FormGroup({
-      FirstName: new FormControl(this.titlecasePipe.transform(this.enrollmentModel.ShippingAddress.FirstName), { validators: [Validators.required, Validators.maxLength(this.firstNameMaxLength)], updateOn: 'blur' }),
-      LastName: new FormControl(this.titlecasePipe.transform(this.enrollmentModel.ShippingAddress.LastName), { validators: [Validators.required, Validators.maxLength(this.lastNameMaxLength)], updateOn: 'blur' }),
-      Street1: new FormControl(this.titlecasePipe.transform(this.enrollmentModel.ShippingAddress.Street1), { validators: [Validators.required, Validators.pattern('^.{1,35}$')], updateOn: 'blur' }),
-      Street2: new FormControl(this.titlecasePipe.transform(this.enrollmentModel.ShippingAddress.Street2), { validators: [Validators.pattern('^.{1,35}$')], updateOn: 'blur' }),
-      City: new FormControl(this.titlecasePipe.transform(this.enrollmentModel.ShippingAddress.City), { validators: [Validators.required, Validators.pattern('^.{1,40}$')], updateOn: 'blur' }),
-      State: new FormControl(this.enrollmentModel.ShippingAddress.State, { validators: [Validators.required], updateOn: 'blur' }),
-      PostalCode: new FormControl(this.enrollmentModel.ShippingAddress.PostalCode, { validators: [Validators.required, Validators.pattern('^[0-9]{5}(?:-[0-9]{4})?$')], updateOn: 'blur' }),
-      isPrimary: new FormControl(this.enrollmentModel.ShippingAddress.Primary)
+      FirstName: new FormControl(this.titlecasePipe.transform(this.demo.ShippingAddress.FirstName), { validators: [Validators.required, Validators.maxLength(this.firstNameMaxLength)], updateOn: 'blur' }),
+      LastName: new FormControl(this.titlecasePipe.transform(this.demo.ShippingAddress.LastName), { validators: [Validators.required, Validators.maxLength(this.lastNameMaxLength)], updateOn: 'blur' }),
+      Street1: new FormControl(this.titlecasePipe.transform(this.demo.ShippingAddress.Street1), { validators: [Validators.required, Validators.pattern('^.{1,35}$')], updateOn: 'blur' }),
+      Street2: new FormControl(this.titlecasePipe.transform(this.demo.ShippingAddress.Street2), { validators: [Validators.pattern('^.{1,35}$')], updateOn: 'blur' }),
+      City: new FormControl(this.titlecasePipe.transform(this.demo.ShippingAddress.City), { validators: [Validators.required, Validators.pattern('^.{1,40}$')], updateOn: 'blur' }),
+      State: new FormControl(this.demo.ShippingAddress.State, { validators: [Validators.required], updateOn: 'blur' }),
+      PostalCode: new FormControl(this.demo.ShippingAddress.PostalCode, { validators: [Validators.required, Validators.pattern('^[0-9]{5}(?:-[0-9]{4})?$')], updateOn: 'blur' }),
+      isPrimary: new FormControl(this.demo.ShippingAddress.Primary)
     }, { updateOn: 'submit' });
 
   }
   
-
-  openSavedAddress() {
-    this.loadSavedAddressComponent = true;
-
-    // Disable Continue and Back buttons when Choose Saved Address drawer is open
-    this.disableContinue = true;
-
-    this.shippingList.toggle();
-  }
-
   onContinue() {
     console.log("submit")
   }
 
-   getSelectedAddress(valueEmitted: { selectedAddress: AddressModel }) {
-  //   // Populate shipping form from the model
-  //   this.savedAddressInfo = valueEmitted?.selectedAddress;
-  //   this.shippingForm.setValue({
-  //     FirstName: this.titlecasePipe.transform(valueEmitted.selectedAddress.FirstName),
-  //     LastName: this.titlecasePipe.transform(valueEmitted.selectedAddress.LastName),
-  //     Street1: this.titlecasePipe.transform(valueEmitted.selectedAddress.Street1),
-  //     Street2: this.titlecasePipe.transform(valueEmitted.selectedAddress.Street2),
-  //     City: this.titlecasePipe.transform(valueEmitted.selectedAddress.City),
-  //     State: valueEmitted.selectedAddress.State,
-  //     PostalCode: valueEmitted.selectedAddress.PostalCode,
-  //     isPrimary: valueEmitted.selectedAddress.Primary
-  //   });
-   }
-
-  onOpenedChange(e: boolean) {
-    // Check for false value. That means mat-drawer was closed.
-    if (e == false) {
-      // Enable Continue and Back buttons when the drawer is closed.
-      this.disableContinue = false;
-    }
-  } 
-
-  prePopulateShippingInfo() {   
-    // this.shippingForm.setValue({
-    //   AddressNumber: "",
-    //   FirstName: 'demo',
-    //   LastName: 'test',
-    //   Street1: "",
-    //   Street2: "",
-    //   City: "",
-    //   State: "",
-    //   PostalCode: "",
-    //   isPrimary: "X"
-    // });
-    // this.shippingForm.patchValue({ isPrimary: "X" }, { emitEvent: false });
-    // this.savedAddressInfo.Primary = "X";
-       
-  }
-
   onKeyDown(event: any, controlName: string) {
     if (event.key === 'Enter') {
-      event.preventDefault(); // Prevent form submission
-      // Update the form control's value
+      event.preventDefault(); 
       this.shippingForm.get(controlName)?.patchValue((event.target as HTMLInputElement).value);
-      // Trigger validation for the specific control
       this.shippingForm.get(controlName)?.markAsTouched();
       this.onContinue();
     }
@@ -144,10 +68,7 @@ export class ShipformComponent implements OnInit {
     this.shippingForm.get(controlName)?.patchValue(event.value);
   }
 
-  //Populate State value when autofill is triggered
   onSateInputChange(event: Event) {
-    console.log("this.shippingForm.controls['State'].value  from autofill menthod", this.shippingForm.controls['State'].value);
-
     const inputElement = event.target as HTMLInputElement;
     const inputValue = inputElement.value;
     const state = this.states.find(state => state.name.toLowerCase() === inputValue.toLowerCase() || state.abbreviation.toLowerCase() === inputValue.toLowerCase());
